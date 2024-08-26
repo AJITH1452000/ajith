325e8c5b72ea4fdedabee5148ff50a8a1dabeb0e
ajith <ajith13422@gmail.com>
2024-08-26 18:13:50 +0530

    first-comment

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import PADDY from "../images/PADDY.jpg";
+import CORN from "../images/CORN.jpg";
+import WHEAT from "../images/WHEAT.jpg";
+import COTTON from "../images/COTTON.jpg";
+import SUGARCANE from "../images/SUGARCANE.jpg";
+import OIL_SEEDS from "../images/OILSEEDS.jpg";
+
+export const MenuList = [
+  {
+    name: "PADDY",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: PADDY,
+    price: 200,
+  },
+  {
+    name: "CORN",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: CORN,
+    price: 250,
+  },
+  {
+    name: "WHEAT",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: WHEAT,
+    price: 300,
+  },
+  {
+    name: "COTTON",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: COTTON,
+    price: 100,
+  },
+  {
+    name: "SUGARCANE",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: SUGARCANE,
+    price: 400,
+  },
+  {
+    name: "OIL SEEDS",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: OIL_SEEDS,
+    price: 500,
+  },
+];
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+import { Link } from "react-router-dom";
+import Banner from "../images/pngtree-golden-rice-field-harvested-in-autumn-png-image_6764195.jpg";
+import "../styles/HomeStyles.css";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
+        <div className="headerContainer">
+          <h1>ELAYA HARVESTING SERVICE</h1>
+          <p>BEST SERVICE IN TAMILNADU</p>
+          <Link to="/menu">
+            <button>BOOK NOW</button>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
